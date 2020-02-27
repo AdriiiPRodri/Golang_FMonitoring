be86ef956fee22d801866e366f3754dff49cf30b
Adrián Jesús Peña Rodríguez <adrianjpr@gmail.com>
2020-02-27 09:05:58 +0100

    Add file structure

---
+++ file_structure.go
@@ -0,0 +1,14 @@
+package main
+
+type File struct {
+	Filename  string
+	Size      string
+	Mode      string
+	ModTime   string
+	IsDir     string
+	Inode     string
+	HLinks    string
+	IsSymLink string
+	RSymLink  string
+	Checksum  string
+}
